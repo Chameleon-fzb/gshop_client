ce4581f6b6b47965c6972041004a4cc5d9bc40a2
chameleon-fzb <1466518303@qq.com>
2021-10-14 16:41:00 +0800

    完善store

--- src/store/index.js
+++ src/store/index.js
@@ -3,30 +3,14 @@ vuex最核心的管理对象store ==> 仓库
 */
 import Vue from 'vue'
 import Vuex from 'vuex'
-import home from './modules/home'
-import user from './modules/user'
+import modules from './modules'
 Vue.use(Vuex)
 
-const mutations = {
-	a(state) {
-		console.log(state)
-	}
-}
-const actions = {
-	b(commit) {
-		console.log(commit)
-	}
-}
-const getters = {
-	c(state) {
-		return state
-	}
-}
 // 向外暴露 store 对象
 export default new Vuex.Store({
 	// state,
-	mutations,
-	actions,
-	getters,
-	modules: { home, user }
+	// mutations,
+	// actions,
+	// getters,
+	modules
 })
--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,21 +1,28 @@
 /* 
 管理首页的数据的 vuex 子模块
 */
-import { reqCategoryList } from '@/api'
+import { reqCategoryList, reqBannerList } from '@/api'
 
 const state = {
 	categoryList: [],
-	a: 1,
-	b: 2
+	bannerList: []
 }
 
 const actions = {
-	// ? 1 异步获取三级分类列表
+	// ? 1 异步 获取 三级分类 列表
 	async getCategoryList({ commit }) {
 		const result = await reqCategoryList()
 		if (result.code === 200) {
 			const cateGoryList = result.data
 			commit('RECEIVE_CATEGORY_LIST', cateGoryList)
+		}
+	},
+	// ? 2 异步获取轮播图 列表
+	async getBannerList({ commit }) {
+		const result = await reqBannerList()
+		if (result.code === 200) {
+			const bannerList = result.data
+			commit('RECEIVE_BANNER_LIST', bannerList)
 		}
 	}
 }
@@ -24,6 +31,10 @@ const mutations = {
 	// ? 1 接收 保存 三级 列表数据
 	RECEIVE_CATEGORY_LIST(state, cateGoryList) {
 		state.categoryList = cateGoryList.splice(0, 15)
+	},
+	// ? 2 接收 保存 轮播图 数据
+	RECEIVE_BANNER_LIST(state, bannerList) {
+		state.bannerList = bannerList
 	}
 }
 
