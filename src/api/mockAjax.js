e1c8a74a7daba9affc35b8a9c80d92d7390d4ea3
chameleon-fzb <1466518303@qq.com>
2021-10-17 00:55:23 +0800

    修改mockAjax 添加进度条

--- src/api/mockAjax.js
+++ src/api/mockAjax.js
@@ -1,20 +1,23 @@
 import axios from 'axios'
-
+import nProgress from 'nprogress'
 const mockAjax = axios.create({
 	baseURL: '/mock',
 	timeout: 10000
 })
 
 mockAjax.interceptors.request.use(config => {
+	nProgress.start()
 	return config
 })
 
 mockAjax.interceptors.response.use(
 	response => {
+		nProgress.done()
 		return response.data
 	},
 	error => {
 		alert(error.message || '未知错误')
+		nProgress.done()
 		return Promise.reject(error)
 	}
 )
