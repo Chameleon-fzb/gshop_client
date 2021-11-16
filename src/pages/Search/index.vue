43347d0e51be3042efd7597f52d582d116bb64b9
chameleon-fzb <1466518303@qq.com>
2021-11-16 17:14:49 +0800

    search 页面 点击商品图片 和名字跳转到detail

--- src/pages/Search/index.vue
+++ src/pages/Search/index.vue
@@ -98,9 +98,9 @@
 							<li class="yui3-u-1-5" v-for="item in goodsList" :key="item.id">
 								<div class="list-wrap">
 									<div class="p-img">
-										<a href="javascript:">
+										<router-link :to="`/detail/${item.id}`">
 											<img :src="item.defaultImg" />
-										</a>
+										</router-link>
 									</div>
 									<div class="price">
 										<strong>
@@ -109,9 +109,9 @@
 										</strong>
 									</div>
 									<div class="attr">
-										<a href="javascript:">
+										<router-link :to="`/detail/${item.id}`">
 											{{ item.title }}
-										</a>
+										</router-link>
 									</div>
 									<div class="commit">
 										<i class="command">
