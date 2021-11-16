26e6cbe1b4036e66917f32e80dd8a788f95fbb7c
chameleon-fzb <1466518303@qq.com>
2021-11-16 13:31:31 +0800

    detail 路由配置

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ import Home from '@/pages/Home'
 import Login from '@/pages/Login'
 import Register from '@/pages/Register'
 import Search from '@/pages/Search'
-
+import Detail from '@/pages/Detail'
 export default [
 	{
 		name: 'home',
@@ -31,5 +31,9 @@ export default [
 		meta: {
 			isHideFooter: true
 		}
+	},
+	{
+		path: '/detail/:skuId',
+		component: Detail
 	}
 ]
