66a031a3ee3deb3dc1252bc83f3498eedabaeaa0
chameleon-fzb <1466518303@qq.com>
2021-12-08 19:46:43 +0800

    使用uuid 为游客用户提供临时id

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,17 @@
 /* 
 管理登陆用户数据的 vuex 子模块
 */
-
+import { getUserTempId } from '@/utils/userAbout'
+const state = {
+	// getUserTempId()//获取临时标识的id
+	userTempId: getUserTempId()
+}
+const mutations = {}
+const actions = {}
+const getters = {}
 export default {
-	state: {},
-	mutations: {},
-	actions: {},
-	getters: {}
+	state,
+	mutations,
+	actions,
+	getters
 }
---
+++ src/utils/userAbout.js
@@ -0,0 +1,14 @@
+// 这个函数式让用户获取唯一id标识的
+// 首先从localStorage当中区获取,如果没有
+// 再调用uuid创建新的标识 还要存在localStorage
+
+import { v4 as uuidv4 } from 'uuid'
+function getUserTempId() {
+	let userTempId = localStorage.getItem('USER_TEMP_ID_KEY')
+	if (!userTempId) {
+		userTempId = uuidv4()
+		localStorage.setItem('USER_TEMP_ID_KEY', userTempId)
+	}
+	return userTempId
+}
+export { getUserTempId }
